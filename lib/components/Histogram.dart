be6340b145cb9c3a8b0b96335d42e8b4db74c4d6
ServOKio <51263003+ServOKio@users.noreply.github.com>
2024-05-22 22:54:21 +0300

    Ops

--- lib/components/Histogram.dart
+++ lib/components/Histogram.dart
@@ -72,7 +72,7 @@ class _HistogramState extends State<Histogram> {
   Future<img.Image?> _calculation(String imagePath, BoxConstraints constraints) async {
     img.Image? data;
     if(false){
-      data = await img.decodeImageFile(imagePath, byExtension: false);
+      data = await img.decodeImageFile(imagePath);
     } else {
       final Uint8List bytes = await compute(readAsBytesSync, imagePath);
       data = img.decodeImage(bytes);
