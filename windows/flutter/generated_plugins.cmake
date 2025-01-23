11287bd7bf549aa5efef7766358f37865e9cd0a9
ServOKio <51263003+ServOKio@users.noreply.github.com>
2025-01-23 16:18:10 +0300

    Moving to gradle 8.3

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -7,7 +7,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   irondash_engine_context
   permission_handler_windows
-  screen_retriever
+  screen_retriever_windows
   share_plus
   sqlite3_flutter_libs
   super_native_extensions
